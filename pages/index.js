aac4edb7bd13e7c6839e103b410cb9ee0af7f837
min <hororonoodle@gmail.com>
2022-01-01 23:44:00 +0900

    fix: map compoent path error

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,3 @@
-import Head from "next/head";
-import Image from "next/image";
 import Banner from "../components/Banner";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
