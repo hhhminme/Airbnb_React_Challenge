d3877d0b422d84f96b2d5e106fd904bfd7687a27
min <hororonoodle@gmail.com>
2022-01-01 23:38:56 +0900

    fix: Map component path error

--- pages/search.js
+++ pages/search.js
@@ -3,7 +3,7 @@ import Footer from "../components/Footer";
 import Header from "../components/Header";
 import { format } from "date-fns";
 import InfoCard from "../components/InfoCard";
-import Map from "../components/map";
+import Map from "../components/Map";
 import Head from "next/head";
 
 function Search({ searchResults }) {
